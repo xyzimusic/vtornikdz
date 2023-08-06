f59fce65cd20b52b8f4ac4a8b8ff66589bc76325
XYZI <113294539+xyzimusic@users.noreply.github.com>
2023-08-06 17:54:04 +0300

    vtornik-dz-4 nedodel

--- src/s2-homeworks/hw04/Stand.tsx
+++ src/s2-homeworks/hw04/Stand.tsx
@@ -43,7 +43,7 @@ const Stand = () => {
             <div className={s.buttons}>
                 {/*обычная кнопка:*/}
                 <div>
-                    <SuperButton id={'hw4-super-button-default'}>
+                    <SuperButton id={'hw4-super-button-default'} >
                         default
                     </SuperButton>
                 </div>
--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -18,7 +18,8 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     }
 ) => {
     const finalClassName = `${s.button} ${disabled ? s.disabled :
-            xType=== 'red' ? s.red : className ? className: ''}`
+            xType=== 'red' ? s.red :
+                xType=== 'secondary' ? s.secondary: s.default}`
                                     // ${xType=== 'red'} ? ${s.red} ${className}?
         // + (disabled
         //         ? ...
