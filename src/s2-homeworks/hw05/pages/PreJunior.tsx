7ff197d746587eccad215f424355d18d07cb93a7
XYZI <113294539+xyzimusic@users.noreply.github.com>
2023-09-12 20:17:48 +0300

    vtornik-dz-6

--- src/s2-homeworks/hw05/pages/PreJunior.tsx
+++ src/s2-homeworks/hw05/pages/PreJunior.tsx
@@ -8,9 +8,9 @@ function PreJunior() {
     return (
         <div id={'hw5-page-pre-junior'}>
             {/*pre junior page*/}
-            <HW1 />
-            <HW2 />
-            <HW3 />
+            {/*<HW1 />*/}
+            {/*<HW2 />*/}
+            {/*<HW3 />*/}
             <HW4 />
         </div>
     )
