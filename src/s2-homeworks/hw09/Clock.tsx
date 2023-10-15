28b2aaaa7335b228def023adb817317a4ee6ce2a
XYZI <113294539+xyzimusic@users.noreply.github.com>
2023-10-15 22:42:50 +0300

    vtornik-dz-9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -34,17 +34,15 @@ function Clock() {
         setShow(false) // Скрываем дату при уходе мыши
     }
 
-    const stringTime = date.toLocaleTimeString(); // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
-    // @ts-ignore
-    const stringDate = date.toLocaleDateString('ru-RU', options).replace(/\./g, '-');// день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringTime = date.toLocaleTimeString('ru-Ru'); // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringDate = date.toLocaleDateString('ru-Ru');// день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = date.toLocaleDateString('en-US', { weekday: 'long' });// пишут студенты
     const stringMonth = date.toLocaleDateString('en-US', { month: 'long' }); // пишут студенты
 
-
-
+    console.log(`stringDate = ${stringDate}`)
+    console.log(`stringTime = ${stringTime}`)
     return (
         <div className={s.clock}>
             <div
