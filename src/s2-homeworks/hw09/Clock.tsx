aecbfeee15ec09fe5be0239b74f69217a2655518
XYZI <113294539+xyzimusic@users.noreply.github.com>
2023-10-15 21:59:13 +0300

    vtornik-dz-9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -29,7 +29,7 @@ function Clock() {
         setShow(false) // Скрываем дату при уходе мыши
     }
 
-    const stringTime = date.toLocaleTimeString(); // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringTime =  date.toLocaleTimeString(undefined, { hour12: false });; // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     const stringDate = date.toLocaleDateString();// день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
